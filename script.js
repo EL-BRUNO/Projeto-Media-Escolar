d32799b4e1cc304132f17b71614ba953ec19c775
EL-BRUNO <brunosoliveira466@gmail.com>
2023-10-28 21:37:44 -0300

    criação de um projeto simples de notas escolares

---
+++ script.js
@@ -0,0 +1,25 @@
+const IputNote = document.querySelector(".note")
+const ButtonFind = document.querySelector(".find-out")
+
+function FindOutNote(){
+  const InpupValue = document.querySelector(".note").value
+  
+  const MediaDosAlunos = InpupValue;
+  const MediaAprovação = 70;
+  
+  if (MediaDosAlunos == MediaAprovação) {
+    alert("Parabéns, foi aprovado");
+  } else if (MediaDosAlunos > 70 && MediaDosAlunos <= 80) {
+    alert("Parabéns, ficou acima da média");
+  } else if (MediaDosAlunos > 80 && MediaDosAlunos <= 90) {
+    alert("Incrível, conseguiu uma nota excelente");
+  } else if (MediaDosAlunos > 90 && MediaDosAlunos < 100) {
+    alert("Maravilhoso, és um dos melhores");
+  } else if (MediaDosAlunos == 100) {
+    alert("Você é demais, conseguiu a nota máxima!");
+  } else if (MediaDosAlunos < 70 && MediaDosAlunos >=51) {
+    alert("Infelizmente você foi reprovado");
+  } else if (MediaDosAlunos >= 0 && MediaDosAlunos <=50){
+    alert('Mais sorte da próxima vez, sei que consegue ser melhor')
+  }
+}
